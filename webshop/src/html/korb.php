5ff9ceb9e9f51a73913eb447978f8740059d78f6
drawig <drawig@gmail.com>
2011-11-12 15:16:28 +0100

    korb.php kann nun nicht mehr aufgerufen werden, wenn man nicht eingeloggt ist.

--- webshop/src/html/korb.php
+++ webshop/src/html/korb.php
@@ -1,6 +1,12 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+<?php
+	session_start();
+
+	if(!isset($_SESSION['user']))
+		header("Location: noaccess.html");
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
