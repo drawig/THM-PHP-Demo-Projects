04b9ade8a54987a43b4287480e64ac9c80628c1d
drawig <drawig@gmail.com>
2011-11-14 18:59:11 +0100

    Warenkorb zum User hinzugefügt.

--- webshop/src/php/User.php
+++ webshop/src/php/User.php
@@ -1,21 +1,27 @@
 <?php
 
+	include('Warenkorb.php');
+
 	/**
 	 * Ein Objekt dieser Klasse repräsentiert einen eingeloggten Benutzer im Webshop.
 	 * Dieser hat einen eindeutigen Benutzernamen, mit dem er erkannt werden kann.
+	 * Außerdem hat ein Benutzer einen Warenkorb, in den er Artikel, die er kaufen möchte,
+	 * einstellen kann.
 	 */
 	class User {
 
 		/** Der eindeutige Name des Benutzers. */
 		private $mName;
+		private $mWarenkorb;
 
 		/**
-		 * Erzeugt ein neues User Objekt mit dem angegebenen Benutzernamen.
+		 * Erzeugt ein neues User Objekt mit dem angegebenen Benutzernamen und einem leeren Warenkorb.
 		 *
 		 * @param string Der Benutzername dieses Benutzers.
 		 */
 		function __construct($name) {
 			$this->mName = $name;
+			$this->mWarenkorb = new Warenkorb();
 		}
 
 		/**
@@ -25,6 +31,15 @@
 		 */
 		public function getName() {
 			return $this->mName;
+		}
+
+		/**
+		 * Gibt den Warenkorb, der zu diesem User gehört zurück.
+		 *
+		 * @return Warenkorb Der Warenkorb, der zu diesem User gehört.
+		 */
+		public function getWarenkorb() {
+			return $this->mWarenkorb;
 		}
 	}
 ?>
