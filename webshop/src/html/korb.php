490a96f7497f3ce0fa14c4a3a1c48a31d27075e7
drawig <drawig@gmail.com>
2011-11-15 15:06:01 +0100

    session_start in korb.php zum Anfang verlegt.

--- webshop/src/html/korb.php
+++ webshop/src/html/korb.php
@@ -1,10 +1,11 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php
+	session_start(); 
+	echo '<?xml version="1.0" encoding="UTF-8"?>'; 
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <?php
 	include('../php/User.php');
-
-	session_start();
 
 	if(!isset($_SESSION['user'])) {
 		header("Location: noaccess.html");
