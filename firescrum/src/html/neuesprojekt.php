8734ff69db8c42d5fbb15db9a0897b49fbaa993f
drawig <drawig@gmail.com>
2011-11-16 11:59:10 +0100

    Formular zum hinzufügen von neuen Projekten in Firescrum hinzugefügt.

---
+++ firescrum/src/html/neuesprojekt.php
@@ -0,0 +1,17 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+	<head>
+		<title>Firescrum - Neues Projekt</title>
+	</head>
+	<body>
+		<h2>Neues Projekt</h2>
+		<form method="post" action="index.php">
+			<h3>Titel:</h3>
+			<input type="text" name="titel"/><br>
+			<h3>Beschreibung:</h3>
+			<textarea name="beschreibung" cols="100" rows="50"/>
+		</form>
+	</body>
+</html>
