e6ad883dff0862a534e7c8700c122720f6e35c07
drawig <drawig@gmail.com>
2011-11-16 11:44:00 +0100

    Startseite zu Firescrum hinzugefügt.

---
+++ firescrum/src/html/index.php
@@ -0,0 +1,13 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+	<head>
+		<title>Firescrum - Startseite</title>
+	</head>
+	<body>
+		<h1>Willkommen bei Firescrum</h1>
+		<a href="neuesprojekt.php">Neues Projekt hinzuf&uuml;gen</a><br/>
+		<a href="projekte.php">Projekte</a>
+	</body>
+</html>
