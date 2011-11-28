efc0df2fcdce7a58f52dd66832b8b8b320be3e6e
drawig <drawig@gmail.com>
2011-11-28 21:46:20 +0100

    Einige Fehler bei Firescrum behoben.

--- firescrum/src/html/showtickets.php
+++ firescrum/src/html/showtickets.php
@@ -10,7 +10,34 @@
 		<?php
 			include('../php/DatabaseAdapter.php');
 
+			if(isset($_POST['projekt'])) {
+				$tickets = DatabaseAdapter::getTickets($_POST['projekt']);
 
+				foreach($tickets as $ticket) {
+					$id = $ticket->getId();
+					$titel = $ticket->getTitel();
+					$stunden = $ticket->getStunden();
+					$beschreibung = $ticket->getBeschreibung();
+					$vorgaenger = $ticket->getVorgaenger();
+					$nachfolger = $ticket->getNachfolger();
+
+					echo "$id<br/>
+						<h4>$titel</h4>&nbsp;&nbsp;&nbsp;&nbsp;$stunden<br/>
+						$beschreibung<br/>
+						<h4>Vorgaenger:</h4>";
+
+					foreach($vorgaenger as $value)
+						echo "$value&nbsp;";
+
+					echo '<br/>
+						<h4>Nachfolger:</h4>';
+
+					foreach($nachfolger as $value)
+						echo "$value&nbsp;";
+
+					echo '<br/>';
+				}
+			}
 		?>
 	</body>
 </html>
